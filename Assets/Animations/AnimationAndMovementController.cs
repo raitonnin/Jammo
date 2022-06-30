edff84af47d74d42c1eedcf3c41bcbdd21f32324
raitonnin <45186265+raitonnin@users.noreply.github.com>
2022-06-30 02:14:27 -0500

    i have like no fuckin idea t
    theres like 8 updated things to push and i ddont know why they all showed up as if i didnt do it at the time

--- Assets/Animations/AnimationAndMovementController.cs
+++ Assets/Animations/AnimationAndMovementController.cs
@@ -5,75 +5,143 @@ using UnityEngine.InputSystem;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
-    PlayerInput playerInput;
+    // declare reference variables
     CharacterController characterController;
     Animator animator;
+    PlayerInput playerInput; // NOTE: PlayerInput class must be generated from New Input System in Inspector -- see 10:30 in this video
 
+    // variables to store optimized setter/getter parameter IDs
+    int isWalkingHash;
+    int isRunningHash;
+
+    // variables to store player input values
     Vector2 currentMovementInput;
     Vector3 currentMovement;
+    Vector3 currentRunMovement;
     bool isMovementPressed;
-    float rotationFactorPerFrame =1.0f;
+    bool isRunPressed;
 
-    void Awake()
+    // constants
+    float rotationFactorPerFrame = 15.0f;
+    float runMultiplier = 3.0f;
+    int zero = 0;
+    float gravity = -9.8f;
+    float groundedGravity = -.05f;
+
+    // Awake is called earlier than Start in Unity's event life cycle
+    void Awake() 
     {
         // initially set reference variables
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-        //set the player input callbacks
+
+        // set the parameter hash references
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+
+        // set the player input callbacks
         playerInput.CharacterControls.Move.started += onMovementInput;
         playerInput.CharacterControls.Move.canceled += onMovementInput;
         playerInput.CharacterControls.Move.performed += onMovementInput;
-
+        playerInput.CharacterControls.Run.started += onRun;
+        playerInput.CharacterControls.Run.canceled += onRun;
     }
-    void HandleRotation()
+
+    void onRun (InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
+    }
+
+    void handleRotation()
     {
         Vector3 positionToLookAt;
-        //the change in position our character should point to
+        // the change in position our character should point to
         positionToLookAt.x = currentMovement.x;
-        positionToLookAt.y = 0.0f;
+        positionToLookAt.y = zero;
         positionToLookAt.z = currentMovement.z;
-        //current rotation of character
+        // the current rotation of our character
         Quaternion currentRotation = transform.rotation;
-        if (isMovementPressed){
-        //creates a new rotation based on where the player is pressing
-        Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
-        Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame *Time.deltaTime); Debug.Log("help");
+
+        if (isMovementPressed) {
+            // creates a new rotation based on where the player is currently pressing
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            // rotate the character to face the positionToLookAt            
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
         }
     }
 
-    void handleAnimation()
-    {
-        bool isWalking = animator.GetBool("isWalking");
-        bool isRunning = animator.GetBool("isRunning");
-
-        if (isMovementPressed && !isWalking) {
-            animator.SetBool("isWalking", true);
-        }
-        else if (!isMovementPressed && isWalking) {
-            animator.SetBool("isWalking", false);
-        } 
-
-    }
+    // handler function to set the player input values
     void onMovementInput (InputAction.CallbackContext context)
     {
-        currentMovementInput = context.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x;
-        currentMovement.z = currentMovementInput.y;
-        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0; 
+            currentMovementInput = context.ReadValue<Vector2>();
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            currentRunMovement.x = currentMovementInput.x * runMultiplier;
+            currentRunMovement.z = currentMovementInput.y * runMultiplier;
+            isMovementPressed = currentMovementInput.x != zero || currentMovementInput.y != zero;
     }
+    
+    void handleAnimation()
+    {
+        // get parameter values from animator
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        // start walking if movement pressed is true and not already walking
+        if (isMovementPressed && !isWalking) {
+            animator.SetBool(isWalkingHash, true);
+        }
+        // stop walking if isMovementPressed is false and not already walking
+        else if (!isMovementPressed && isWalking) {
+            animator.SetBool(isWalkingHash, false);
+        }
+        // run if movement and run pressed are true and not currently running
+        if ((isMovementPressed && isRunPressed) && !isRunning)
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        // stop running if movement or run pressed are false and currently running
+        else if ((!isMovementPressed || !isRunPressed) && isRunning) {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+
+    void handleGravity()
+    {
+        // apply proper gravity if the player is grounded or not
+        if (characterController.isGrounded) {
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
+        } else {
+            currentMovement.y += gravity;
+            currentRunMovement.y += gravity;
+        }
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        HandleRotation();
+        handleGravity();
+        handleRotation();
         handleAnimation();
-        characterController.Move(currentMovement * Time.deltaTime);
+
+        if (isRunPressed) {
+            characterController.Move(currentRunMovement * Time.deltaTime);
+        } else {
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
     }
+    
     void OnEnable()
     {
+        // enable the character controls action map
         playerInput.CharacterControls.Enable();
     }
+
     void OnDisable()
     {
+        // disable the character controls action map
         playerInput.CharacterControls.Disable();
     }
 }
